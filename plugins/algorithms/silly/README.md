a391f102da0766fd3f3f34fd24d1baffda52a834
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-08-04 21:45:55 -0700

    Add a random search algorithm plugin

---
+++ plugins/algorithms/silly/README.md
@@ -0,0 +1,7 @@
+# Silly Algorithm for Badger
+
+Just a super silly random search optimization algorithm. The sole usage of this is to demo the functionality of Badger's algorithm plugin system.
+
+## Prerequisites
+
+## Usage
---
+++ plugins/algorithms/silly/algorithm.py
@@ -0,0 +1,16 @@
+import numpy as np
+from operator import itemgetter
+
+def optimize(evaluate, params):
+    D, max_iter = itemgetter('dimension', 'max_iter')(params)
+
+    x_opt = None
+    y_opt = -np.Inf
+    for i in range(max_iter):
+        x = np.random.rand(D).reshape(1, -1)
+        y = evaluate(x)
+        if y < y_opt:  # assume a minimize problem
+            x_opt = x
+            y_opt = y
+
+    return y_opt, x_opt
