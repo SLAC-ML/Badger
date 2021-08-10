7f8859b2680c676825f332bdeb8a2e0f097369af
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-08-09 23:46:33 -0700

    Update the silly interface w/ configs

--- plugins/interfaces/silly/interface.py
+++ plugins/interfaces/silly/interface.py
@@ -1,31 +1,31 @@
 from ....badger import interface
+from operator import itemgetter
 import logging
 
 class Interface(interface.Interface):
 
     name = 'silly'
-    states = {
-        'c1': 0,
-        'c2': 1,
-        'c3': 2,
-        'c4': 3
-    }
 
-    def get_values(self, channels: list[str]) -> list:
-        values = []
-        for c in channels:
-            try:
-                values.append(self.states[c])
-            except KeyError:
-                values.append(None)
+    def __init__(self, params):
+        super().__init__(params)
 
-        return values
+        prefix, count = itemgetter('channel_prefix', 'channel_count')(params)
 
-    def set_values(self, channels: list[str], values: list):
-        assert len(channels) == len(values), 'Channels and values number mismatch!'
+        self.states = {}
+        for i in range(count):
+            self.states[f'{prefix}{i + 1}'] = 0
 
-        for idx, c in enumerate(channels):
-            try:
-                self.states[c] = values[idx]
-            except KeyError:
-                logging.warn(f'Channel {c} doesn\'t exist!')
+    def get_value(self, channel: str):
+        try:
+            value = self.states[channel]
+        except KeyError:
+            logging.warn(f'Channel {channel} doesn\'t exist!')
+            value = None
+
+        return value
+
+    def set_value(self, channel: str, value):
+        try:
+            self.states[channel] = value
+        except KeyError:
+            logging.warn(f'Channel {channel} doesn\'t exist!')
