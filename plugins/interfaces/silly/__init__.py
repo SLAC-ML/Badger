9d059ff83f6a3bc13291c5bca0419be917c7c326
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-08-25 10:45:35 -0700

    Add some calculation in the silly interface
    Also rename it to enable the direct importing capability

--- plugins/interfaces/silly/__init__.py
+++ plugins/interfaces/silly/__init__.py
@@ -1,4 +1,5 @@
-from ....badger import interface
+import numpy as np
+from badger import interface
 from operator import itemgetter
 import logging
 
@@ -11,9 +12,14 @@ class Interface(interface.Interface):
 
         prefix, count = itemgetter('channel_prefix', 'channel_count')(params)
 
+        self.channels = []
         self.states = {}
         for i in range(count):
+            self.channels.append(f'{prefix}{i + 1}')
             self.states[f'{prefix}{i + 1}'] = 0
+
+        self.channels.append('norm')
+        self.states['norm'] = 0
 
     def get_value(self, channel: str):
         try:
@@ -27,5 +33,7 @@ class Interface(interface.Interface):
     def set_value(self, channel: str, value):
         try:
             self.states[channel] = value
+            values = np.array([self.states[channel] for channel in self.channels[:-1]])
+            self.states['norm'] = np.sqrt(np.sum(values ** 2))
         except KeyError:
             logging.warn(f'Channel {channel} doesn\'t exist!')
