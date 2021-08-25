8086babd71af332ebc4fb4aff6fba386488e24d5
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-08-25 10:46:10 -0700

    Make the badger core a package

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='badger-opt',
+    version='0.1',
+    description='Core of the Badger optimizer',
+    url='https://github.com/SLAC-ML/Badger',
+    author='Zhe Zhang',
+    author_email='zhezhang@slac.stanford.edu',
+    license='GPL',
+    package_dir={'': 'badger'},
+    packages=find_packages(where='badger'),
+    install_requires=[
+        'numpy'
+    ],
+    python_requires='>=3.6',
+)
