cf0ac07a85bb90f3ba3b5151734c71be6c8f71ec
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-08-25 15:09:39 -0700

    Use lazy loading to prevent circular dependencies

--- badger/factory.py
+++ badger/factory.py
@@ -4,114 +4,86 @@ import importlib
 import yaml
 import logging
 
-sys.path.append('D:/Projects/Badger-Plugins')
+BADGER_PLUGIN_ROOT = 'D:/Projects/Badger-Plugins'
+sys.path.append(BADGER_PLUGIN_ROOT)
 
 
-def scan_algorithms(root):
-    factory_algo = {}
+def scan_plugins(root):
+    factory = {}
+    for ptype in ['algorithm', 'interface', 'environment']:
+        factory[ptype] = {}
 
-    algorithms = [fname for fname in os.listdir(root)
-                  if os.path.exists(os.path.join(root, fname, '__init__.py'))]
-    for algo in algorithms:
-        module = importlib.import_module(f'algorithms.{algo}')
-        # Load the params in the configs
-        configs = None
-        with open(os.path.join(root, algo, 'configs.yaml'), 'r') as f:
-            try:
-                configs = yaml.safe_load(f)
-            except yaml.YAMLError:
-                logging.error(
-                    f'Error loading plugin algorithm {algo}: invalid config')
-        if configs is None:
-            params = None
-        else:
-            try:
-                params = configs['params']
-            except KeyError:
-                params = None
-        factory_algo[algo] = [module.optimize, params]
+        proot = os.path.join(root, f'{ptype}s')
 
-    return factory_algo
-
-
-def scan_interfaces(root):
-    factory_intf = {}
-
-    interfaces = [fname for fname in os.listdir(root)
-                  if os.path.exists(os.path.join(root, fname, '__init__.py'))]
-    for intf in interfaces:
-        module = importlib.import_module(f'interfaces.{intf}')
-        # Load the params in the configs
-        configs = None
-        with open(os.path.join(root, intf, 'configs.yaml'), 'r') as f:
-            try:
-                configs = yaml.safe_load(f)
-            except yaml.YAMLError:
-                logging.error(
-                    f'Error loading plugin interface {intf}: invalid config')
-        if configs is None:
-            params = None
-        else:
-            try:
-                params = configs['params']
-            except KeyError:
-                params = None
-        factory_intf[intf] = [module.Interface, params]
-
-    return factory_intf
-
-
-def scan_environments(root):
-    factory_env = {}
-
-    environments = [fname for fname in os.listdir(root)
-                    if os.path.exists(os.path.join(root, fname, '__init__.py'))]
-    for env in environments:
-        module = importlib.import_module(f'environments.{env}')
-        # Load the params in the configs
-        configs = None
-        with open(os.path.join(root, env, 'configs.yaml'), 'r') as f:
-            try:
-                configs = yaml.safe_load(f)
-            except yaml.YAMLError:
-                logging.error(
-                    f'Error loading plugin environment {env}: invalid config')
-        if configs is None:
-            params = None
-        else:
-            try:
-                params = configs['params']
-            except KeyError:
-                params = None
-        factory_env[env] = [module.Environment, params]
-
-    return factory_env
-
-
-def scan_plugins():
-    factory_algo = scan_algorithms('D:/Projects/Badger-Plugins/algorithms')
-    factory_intf = scan_interfaces('D:/Projects/Badger-Plugins/interfaces')
-    factory_env = scan_environments('D:/Projects/Badger-Plugins/environments')
-
-    factory = {
-        'algorithm': factory_algo,
-        'interface': factory_intf,
-        'environment': factory_env,
-    }
+        plugins = [fname for fname in os.listdir(proot)
+                   if os.path.exists(os.path.join(proot, fname, '__init__.py'))]
+        for pname in plugins:
+            factory[ptype][pname] = None
 
     return factory
 
 
+def load_plugin(root, pname, ptype):
+    assert ptype in ['algorithm', 'interface',
+                     'environment'], f'Invalid plugin type {ptype}'
+
+    proot = os.path.join(root, f'{ptype}s')
+
+    # Load the params in the configs
+    configs = None
+    with open(os.path.join(proot, pname, 'configs.yaml'), 'r') as f:
+        try:
+            configs = yaml.safe_load(f)
+        except yaml.YAMLError:
+            logging.error(
+                f'Error loading plugin {ptype} {pname}: invalid config')
+    if configs is None:
+        params = None
+    else:
+        try:
+            params = configs['params']
+        except KeyError:
+            params = None
+
+    # Load module
+    module = importlib.import_module(f'{ptype}s.{pname}')
+
+    if ptype == 'algorithm':
+        plugin = [module.optimize, params]
+    elif ptype == 'interface':
+        plugin = [module.Interface, params]
+    elif ptype == 'environment':
+        plugin = [module.Environment, params]
+
+    BADGER_FACTORY[ptype][pname] = plugin
+
+    return plugin
+
+
+def get_plug(root, name, ptype):
+    try:
+        plug = BADGER_FACTORY[ptype][name]
+        if plug is None:  # lazy loading
+            plug = load_plugin(root, name, ptype)
+            BADGER_FACTORY[ptype][name] = plug
+    except KeyError:
+        logging.error(
+            f'Error loading plugin {ptype} {name}: plugin not found')
+        plug = None
+
+    return plug
+
+
 def get_algo(name):
-    return BADGER_FACTORY['algorithm'][name]
+    return get_plug(BADGER_PLUGIN_ROOT, name, 'algorithm')
 
 
 def get_intf(name):
-    return BADGER_FACTORY['interface'][name]
+    return get_plug(BADGER_PLUGIN_ROOT, name, 'interface')
 
 
 def get_env(name):
-    return BADGER_FACTORY['environment'][name]
+    return get_plug(BADGER_PLUGIN_ROOT, name, 'environment')
 
 
-BADGER_FACTORY = scan_plugins()
+BADGER_FACTORY = scan_plugins(BADGER_PLUGIN_ROOT)
