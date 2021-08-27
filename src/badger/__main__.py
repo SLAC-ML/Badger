256378fa4aa68dbb80667dd68526e3ead670591f
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-08-27 00:34:44 -0700

    Implement the basic CLI usage of Badger

--- src/badger/__main__.py
+++ src/badger/__main__.py
@@ -1,20 +1,45 @@
-from .factory import get_algo, get_intf, get_env
+import argparse
+
+from .actions.doctor import self_check
+from .actions.routine import show_routine
+from .actions.algo import show_algo
+from .actions.env import show_env
+from .actions.intf import show_intf
+from .actions.run import run_routine
 
 
-def main(args=None):
-    Interface, params_intf = get_intf('silly')
-    Environment, params_env = get_env('silly')
-    optimize, params_algo = get_algo('silly')
+def main():
+    # Create the top-level parser
+    parser = argparse.ArgumentParser(description='Badger the optimizer')
+    subparsers = parser.add_subparsers(help='Badger commands help')
 
-    params_algo['dimension'] = 3
-    params_algo['max_iter'] = 10
-    params_intf['channel_count'] = 3
-    params_env['dimension'] = 3
+    # Parser for the 'doctor' command
+    parser_doctor = subparsers.add_parser(
+        'doctor', help='Badger status self-check')
+    parser_doctor.set_defaults(func=self_check)
 
-    intf = Interface(params_intf)
-    env = Environment(intf, params_env)
-    y_opt, x_opt = optimize(env.evaluate, params_algo)
-    print(f'best! {x_opt}: {y_opt}')
+    # Parser for the 'routine' command
+    parser_routine = subparsers.add_parser('routine', help='Badger routines')
+    parser_routine.set_defaults(func=show_routine)
+
+    # Parser for the 'algo' command
+    parser_algo = subparsers.add_parser('algo', help='Badger algorithms')
+    parser_algo.set_defaults(func=show_algo)
+
+    # Parser for the 'intf' command
+    parser_intf = subparsers.add_parser('intf', help='Badger interfaces')
+    parser_intf.set_defaults(func=show_intf)
+
+    # Parser for the 'env' command
+    parser_env = subparsers.add_parser('env', help='Badger environments')
+    parser_env.set_defaults(func=show_env)
+
+    # Parser for the 'run' command
+    parser_run = subparsers.add_parser('run', help='Run routines')
+    parser_run.set_defaults(func=run_routine)
+
+    args = parser.parse_args()
+    args.func(args)
 
 
 if __name__ == '__main__':
