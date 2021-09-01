3b3cc461166f451347858de2b6e710ae708fc2b3
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-09-01 14:08:49 -0700

    Generate the normalized evaluate based on configs

--- src/badger/actions/run.py
+++ src/badger/actions/run.py
@@ -1,3 +1,4 @@
+import numpy as np
 from ..factory import get_algo, get_intf, get_env
 
 
@@ -6,15 +7,40 @@ def run_routine(args):
     Environment, configs_env = get_env('silly')
     optimize, configs_algo = get_algo('silly')
 
+    configs_routine = {
+        'variables': ['q1', 'q2'],
+        'objectives': [{'l1': 'MINIMIZE'}],
+        'constraints': None,
+    }
+
     params_algo = configs_algo['params'].copy()
     params_intf = configs_intf['params'].copy()
-    params_env = configs_env['params'].copy()
-    params_algo['dimension'] = 4
-    params_algo['max_iter'] = 10
+    params_env = None if not configs_env['params'] else configs_env['params'].copy()
+    params_algo['dimension'] = len(configs_routine['variables'])
+    params_algo['max_iter'] = 100
+    params_intf['channel_prefix'] = 'c'
     params_intf['channel_count'] = 4
-    params_env['dimension'] = 4
 
     intf = Interface(params_intf)
     env = Environment(intf, params_env)
-    y_opt, x_opt = optimize(env.evaluate, params_algo)
+    # Make a normalized evaluate function
+    def evaluate(X):
+        Y = []
+        for x in X:
+            env.set_vars(configs_routine['variables'], x)
+            obses = []
+            for obj in configs_routine['objectives']:
+                key = list(obj.keys())[0]
+                ptype = list(obj.values())[0]
+                obs = env.get_obs(key)
+                if ptype == 'MAXIMIZE':
+                    obses.append(-obs)
+                else:
+                    obses.append(obs)
+            Y.append(obses)
+        Y = np.array(Y)
+
+        return Y, None, None
+
+    y_opt, x_opt = optimize(evaluate, params_algo)
     print(f'best! {x_opt}: {y_opt}')
