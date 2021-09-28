eee2fe8526bc11e4e71df89b7e1b7a00d55c138f
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-09-28 14:10:43 -0700

    Add a preliminary extension system

---
+++ src/badger/extension.py
@@ -0,0 +1,29 @@
+import numpy as np
+from abc import ABC, abstractmethod
+
+
+class Extension(ABC):
+
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    # List all available algorithms
+    @abstractmethod
+    def list_algo(self) -> list[str]:
+        pass
+
+    # Get config of an algorithm
+    @abstractmethod
+    def get_algo_config(self, name: str):
+        pass
+
+    # Run an optimization on the given environment
+    @abstractmethod
+    def run(self, env, configs: dict):
+        pass
