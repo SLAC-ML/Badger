9c4517a4b1b61c669f46eaa0a991c5d962842efb
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-09-28 22:17:11 -0700

    Add extension info to the metadata
    Also using yaml printing format rather than a customized one

--- src/badger/actions/__init__.py
+++ src/badger/actions/__init__.py
@@ -1,10 +1,21 @@
 from pkg_resources import get_distribution
-from ..factory import BADGER_PLUGIN_ROOT
+from ..factory import BADGER_PLUGIN_ROOT, BADGER_EXTENSIONS
+from ..utils import yprint
 
 
 def show_info(args):
-    version = get_distribution('badger-opt').version
-    print(f'Badger the optimizer')
-    print('====================')
-    print(f'version      : {version}')
-    print(f'plugins root : {BADGER_PLUGIN_ROOT}')
+    info = {
+        'name': 'Badger the optimizer',
+        'version': get_distribution('badger-opt').version,
+        'plugin root': BADGER_PLUGIN_ROOT,
+    }
+
+    extensions = list(BADGER_EXTENSIONS.keys())
+    if extensions:
+        info['extensions'] = extensions
+
+    yprint(info)
+    # print(f'Badger the optimizer')
+    # print('====================')
+    # print(f'version      : {version}')
+    # print(f'plugins root : {BADGER_PLUGIN_ROOT}')
