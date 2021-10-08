87e1c7e4755233f2cc3120d1c129ca469b05fc0f
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-10-08 10:53:40 -0700

    Add screen logger to show opt progress in console
    The code is mostly borrowed from the bayes_opt package, with tweaks to
    make it work with any optimization task.

---
+++ src/badger/logger/util.py
@@ -0,0 +1,68 @@
+# Borrowed from bayes_opt
+class Colours:
+    """Print in nice colours."""
+
+    BLUE = '\033[94m'
+    BOLD = '\033[1m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    END = '\033[0m'
+    GREEN = '\033[92m'
+    PURPLE = '\033[95m'
+    RED = '\033[91m'
+    UNDERLINE = '\033[4m'
+    YELLOW = '\033[93m'
+
+    @classmethod
+    def _wrap_colour(cls, s, colour):
+        return colour + s + cls.END
+
+    @classmethod
+    def black(cls, s):
+        """Wrap text in black."""
+        return cls._wrap_colour(s, cls.END)
+
+    @classmethod
+    def blue(cls, s):
+        """Wrap text in blue."""
+        return cls._wrap_colour(s, cls.BLUE)
+
+    @classmethod
+    def bold(cls, s):
+        """Wrap text in bold."""
+        return cls._wrap_colour(s, cls.BOLD)
+
+    @classmethod
+    def cyan(cls, s):
+        """Wrap text in cyan."""
+        return cls._wrap_colour(s, cls.CYAN)
+
+    @classmethod
+    def darkcyan(cls, s):
+        """Wrap text in darkcyan."""
+        return cls._wrap_colour(s, cls.DARKCYAN)
+
+    @classmethod
+    def green(cls, s):
+        """Wrap text in green."""
+        return cls._wrap_colour(s, cls.GREEN)
+
+    @classmethod
+    def purple(cls, s):
+        """Wrap text in purple."""
+        return cls._wrap_colour(s, cls.PURPLE)
+
+    @classmethod
+    def red(cls, s):
+        """Wrap text in red."""
+        return cls._wrap_colour(s, cls.RED)
+
+    @classmethod
+    def underline(cls, s):
+        """Wrap text in underline."""
+        return cls._wrap_colour(s, cls.UNDERLINE)
+
+    @classmethod
+    def yellow(cls, s):
+        """Wrap text in yellow."""
+        return cls._wrap_colour(s, cls.YELLOW)
