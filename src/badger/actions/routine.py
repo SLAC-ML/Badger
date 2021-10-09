71b3a69b60b13f3af5244459826cddea1a167971
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-10-09 00:10:12 -0700

    Implement the run saved routine feature

--- src/badger/actions/routine.py
+++ src/badger/actions/routine.py
@@ -1,12 +1,22 @@
 from ..db import load_routine, list_routines
-from ..utils import range_to_str, yprint
+from ..utils import range_to_str, yprint, run_routine
 
 
 def show_routine(args):
+    # List routines
     if args.routine_name is None:
         yprint(list_routines())
-    else:
-        routine = load_routine(args.routine_name)
-        if routine is not None:
-            routine['config']['variables'] = range_to_str(routine['config']['variables'])
-            yprint(routine)
+        return
+
+    routine = load_routine(args.routine_name)
+    if routine is None:
+        return
+
+    # Print the routine
+    if not args.run:
+        routine['config']['variables'] = range_to_str(routine['config']['variables'])
+        yprint(routine)
+        return
+
+    # Run the routine
+    run_routine(routine, args.yes, None, args.verbose)
