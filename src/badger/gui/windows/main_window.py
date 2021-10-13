4260b8ac394435af10d25147c6740a32c39a6523
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-10-12 21:00:40 -0700

    Add logic of multi-page to main window

--- src/badger/gui/windows/main_window.py
+++ src/badger/gui/windows/main_window.py
@@ -1,6 +1,7 @@
 from pkg_resources import get_distribution
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QStackedWidget, QWidget
 from ..pages.home_page import BadgerHomePage
+from ..pages.routine_page import BadgerRoutinePage
 
 
 class BadgerMainWindow(QMainWindow):
@@ -8,17 +9,26 @@ class BadgerMainWindow(QMainWindow):
         super().__init__()
 
         self.init_ui()
+        self.config_logic()
 
     def init_ui(self):
         version = get_distribution('badger-opt').version
         self.setWindowTitle(f'Badger v{version}')
-        # self.setWindowIcon(QIcon('qt.png'))
+        # self.setWindowIcon(QIcon('badger.png'))
         self.resize(640, 480)
         self.center()
 
-        # Add home page
-        home = BadgerHomePage()
-        self.setCentralWidget(home)
+        # Add pages
+        self.home_page = BadgerHomePage()
+        self.routine_page = BadgerRoutinePage(None)
+
+        self.stacks = stacks = QStackedWidget()
+        stacks.addWidget(self.home_page)
+        stacks.addWidget(self.routine_page)
+
+        stacks.setCurrentIndex(0)
+
+        self.setCentralWidget(self.stacks)
 
     def center(self):
         qr = self.frameGeometry()
@@ -26,3 +36,13 @@ class BadgerMainWindow(QMainWindow):
 
         qr.moveCenter(cp)
         self.move(qr.topLeft())
+
+    def config_logic(self):
+        self.home_page.btn_new.clicked.connect(self.show_routine_page)
+        self.routine_page.btn_back.clicked.connect(self.show_home_page)
+
+    def show_routine_page(self):
+        self.stacks.setCurrentIndex(1)
+
+    def show_home_page(self):
+        self.stacks.setCurrentIndex(0)
