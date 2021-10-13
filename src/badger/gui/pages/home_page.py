123c4131635006f36beda12dc543c70db64feb00
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-10-12 20:59:51 -0700

    Fill in the real routines data in home page

--- src/badger/gui/pages/home_page.py
+++ src/badger/gui/pages/home_page.py
@@ -1,7 +1,9 @@
-from PyQt6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy
-from PyQt6.QtWidgets import QPushButton, QLabel
+from datetime import datetime
+from PyQt6.QtWidgets import QLabel, QWidget, QVBoxLayout, QHBoxLayout, QSizePolicy
+from PyQt6.QtWidgets import QPushButton, QGroupBox, QListWidgetItem, QListWidget
 from PyQt6.QtGui import QIcon
 from ..components.search_bar import search_bar
+from ...db import list_routines
 
 
 class BadgerHomePage(QWidget):
@@ -11,6 +13,8 @@ class BadgerHomePage(QWidget):
         self.init_ui()
 
     def init_ui(self):
+        routines, timestamps = list_routines()
+
         # Set up the layout
         vbox = QVBoxLayout(self)
 
@@ -18,7 +22,7 @@ class BadgerHomePage(QWidget):
         panel_search = QWidget()
         hbox_search = QHBoxLayout(panel_search)
 
-        sbar = search_bar()
+        sbar = search_bar(routines)
         btn_setting = QPushButton('Settings')
         hbox_search.addWidget(sbar)
         hbox_search.addWidget(btn_setting)
@@ -26,43 +30,42 @@ class BadgerHomePage(QWidget):
         vbox.addWidget(panel_search)
 
         # Recent routines
-        panel_recent = QWidget()
-        vbox_recent = QVBoxLayout(panel_recent)
+        group_recent = QGroupBox('Recent Routines')
+        hbox_recent = QHBoxLayout(group_recent)
 
-        label_recent = QLabel('Recent Routines')
-        vbox_recent.addWidget(label_recent)
+        self.btn_new = btn_new = QPushButton('+')
+        btn_new.setMinimumHeight(64)
+        btn_new.setFixedWidth(64)
+        hbox_recent.addWidget(btn_new)
 
-        hbox_recent = QHBoxLayout()
-        btn1 = QPushButton('+')
-        btn1.setMinimumHeight(64)
-        btn2 = QPushButton('lcls emittance')
-        btn2.setMinimumHeight(64)
-        btn3 = QPushButton('spear3 lossrate')
-        btn3.setMinimumHeight(64)
-        hbox_recent.addWidget(btn1)
-        hbox_recent.addWidget(btn2)
-        hbox_recent.addWidget(btn3)
-        vbox_recent.addLayout(hbox_recent)
+        for routine in routines[:-4:-1]:
+            btn = QPushButton(routine)
+            btn.setMinimumHeight(64)
+            hbox_recent.addWidget(btn)
 
-        vbox.addWidget(panel_recent)
+        vbox.addWidget(group_recent)
 
         # All routines
-        panel_all = QWidget()
-        vbox_all = QVBoxLayout(panel_all)
+        group_all = QGroupBox('All Routines')
+        routine_list = QListWidget()
+        vbox_all = QVBoxLayout(group_all)
+        vbox_all.addWidget(routine_list)
 
-        label_all = QLabel('All Routines')
-        vbox_all.addWidget(label_all)
+        for i, routine in enumerate(routines):
+            routine_widget = QWidget()
+            routine_layout = QHBoxLayout()
+            routine_widget.setLayout(routine_layout)
+            timestamp = datetime.fromisoformat(timestamps[i])
+            time_str = timestamp.strftime('%m/%d/%Y, %H:%M:%S')
+            btn = QPushButton(f'{routine}: {time_str}')
+            btn.setMinimumHeight(24)
+            routine_layout.addWidget(btn)
+            item = QListWidgetItem(routine_list)
+            item.setSizeHint(routine_widget.sizeHint())
+            routine_list.addItem(item)
+            routine_list.setItemWidget(item, btn)
 
-        btn1 = QPushButton('lcls emittance')
-        btn2 = QPushButton('spear3 lossrate')
-        btn3 = QPushButton('test')
-
-        vbox_all.addWidget(btn1)
-        vbox_all.addWidget(btn2)
-        vbox_all.addWidget(btn3)
-        vbox_all.addStretch()
-
-        vbox.addWidget(panel_all)
+        vbox.addWidget(group_all)
 
         # stylesheet = (
         #     'background-color: red;'
