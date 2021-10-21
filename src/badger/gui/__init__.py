5e1273cfc93f1ba21c4a0e5c3790a478c71c0a2a
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-10-21 12:32:28 -0700

    Support ctrl/cmd+c to quit the GUI

--- src/badger/gui/__init__.py
+++ src/badger/gui/__init__.py
@@ -1,3 +1,5 @@
+import signal
+import time
 from PyQt5.QtWidgets import QApplication
 from PyQt5 import QtCore
 import sys
@@ -18,16 +20,42 @@ if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
 #     QApplication.setAttribute(
 #         QtCore.Qt.HighDpiScaleFactorRoundingPolicy.PassThrough)
 
+TIMER = {'time': None}
+
+
+def on_exit(*args):
+    if TIMER['time'] is None:
+        print('Press Ctrl/Cmd + C again within 1s to quit Badger')
+        TIMER['time'] = time.time()
+        return
+
+    TIMER['time'] = None
+    print('Goodbye')
+    QApplication.quit()
+
+
+def on_timeout():
+    if TIMER['time'] is None:
+        return
+
+    now = time.time()
+    if (now - TIMER['time']) > 1:
+        TIMER['time'] = None
+        print('Timeout, resume the operation...')
+
 
 def launch_gui():
     app = QApplication(sys.argv)
 
+    # Show the main window
     window = BadgerMainWindow()
-
     window.show()
 
-    # timer = QtCore.QTimer()
-    # timer.timeout.connect(lambda: None)
-    # timer.start(100)
+    signal.signal(signal.SIGINT, on_exit)
+
+    # Let the interpreter run each 0.2 s
+    timer = QtCore.QTimer()
+    timer.timeout.connect(on_timeout)
+    timer.start(200)
 
     sys.exit(app.exec())
