97cd41919e513bda23bfc9b6be7d1b81cd7417bc
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-11-08 17:59:27 -0800

    Introduce a proper logging scheme

---
+++ src/badger/log.py
@@ -0,0 +1,31 @@
+from logging.config import dictConfig
+from .utils import merge_params
+
+
+LOG_CONFIG_DEFAULT = {
+    'version': 1,
+    'formatters': {
+        'simple': {
+            'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'level': 'DEBUG',
+            'formatter': 'simple',
+            'stream': 'ext://sys.stdout'
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': [
+            'console'
+        ]
+    }
+}
+
+
+def config_log(dict_config=None):
+    _config = merge_params(LOG_CONFIG_DEFAULT, dict_config)
+    dictConfig(_config)
