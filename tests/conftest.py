ada7e17aef09654f7b820102a0d724859f75836f
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-11-12 21:20:10 -0800

    Add a clean up fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import pytest
 
 
@@ -26,6 +27,21 @@ def config_test_settings(mock_plugin_root, mock_db_root,
     write_value('BADGER_DB_ROOT', old_db)
     write_value('BADGER_LOGBOOK_ROOT', old_logbook)
     write_value('BADGER_ARCHIVE_ROOT', old_archived)
+
+
+@pytest.fixture(scope='module', autouse=True)
+def clean_up(mock_db_root, mock_logbook_root, mock_archive_root):
+    # Clean before tests
+    shutil.rmtree(mock_db_root, True)  # ignore errors
+    shutil.rmtree(mock_logbook_root, True)
+    shutil.rmtree(mock_archive_root, True)
+
+    yield
+
+    # Clean after tests
+    shutil.rmtree(mock_db_root, True)
+    shutil.rmtree(mock_logbook_root, True)
+    shutil.rmtree(mock_archive_root, True)
 
 
 @pytest.fixture(scope='module')
