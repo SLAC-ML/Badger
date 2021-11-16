881a2c698da06434e1c3e916a154a912d2a87464
Zhe Zhang <zhezhang@slac.stanford.edu>
2021-11-16 15:14:11 -0800

    Using subprocess instead of pytester for cli tests

--- src/badger/tests/test_cli_basic.py
+++ src/badger/tests/test_cli_basic.py
@@ -1,19 +1,26 @@
 import os
+import subprocess
 from pkg_resources import get_distribution
 
 
-pytest_plugins = ['pytester']
+def capture(command):
+    proc = subprocess.Popen(
+        command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    out = out.decode('utf-8')
+    err = err.decode('utf-8')
+    return out, err, proc.returncode
 
 
-def test_cli_main(testdir):
-    args = ['badger']
-    result = testdir.run(*args)
+def test_cli_main():
+    command = ['badger']
+    out, err, exitcode = capture(command)
 
-    assert result.ret == 0
+    assert exitcode == 0
 
     # Check output lines
-    outlines = result.outlines
-    assert len(outlines) == 6
+    outlines = out.split('\n')
+    assert len(outlines) == 7
 
     # Check name
     assert outlines[0] == 'name: Badger the optimizer'
@@ -23,17 +30,17 @@ def test_cli_main(testdir):
     assert outlines[1] == f'version: {version}'
 
 
-def test_cli_run(testdir, mock_config_root):
-    args = ['badger', 'run', '-a', 'silly', '-ap',
-            '{dimension: 2, max_iter: 10}',  '-e', 'silly', '-c',
-            os.path.join(mock_config_root, 'test.yaml'), '-y']
-    result = testdir.run(*args)
+def test_cli_run(mock_config_root):
+    command = ['badger', 'run', '-a', 'silly', '-ap',
+               '{dimension: 2, max_iter: 10}',  '-e', 'silly', '-c',
+               os.path.join(mock_config_root, 'test.yaml'), '-y']
+    out, err, exitcode = capture(command)
 
-    assert result.ret == 0
+    assert exitcode == 0
 
     # Check output lines
-    outlines = result.outlines
-    assert len(outlines) == 14
+    outlines = out.split('\n')
+    assert len(outlines) == 15
 
     # Check table header
     assert outlines[1] == '|    iter    |     l2     |     q1     |     q2     |'
