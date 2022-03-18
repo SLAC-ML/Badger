0395364000bce99cd2d89286e06d35ee03a418a9
Zhe Zhang <zhezhang@slac.stanford.edu>
2022-03-18 10:35:50 -0700

    Add copyable data table

---
+++ src/badger/gui_acr/components/data_table.py
@@ -0,0 +1,61 @@
+from PyQt5.QtWidgets import QApplication, QTableWidget, QTableWidgetItem, QHeaderView
+from PyQt5.QtCore import Qt
+
+
+# https://stackoverflow.com/questions/60715462/how-to-copy-and-paste-multiple-cells-in-qtablewidget-in-pyqt5
+class TableWithCopy(QTableWidget):
+    """
+    this class extends QTableWidget
+    * supports copying multiple cell's text onto the clipboard
+    * formatted specifically to work with multiple-cell paste into programs
+      like google sheets, excel, or numbers
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def keyPressEvent(self, event):
+        super().keyPressEvent(event)
+        if event.key() == Qt.Key.Key_C and (event.modifiers() & Qt.KeyboardModifier.ControlModifier):
+            copied_cells = sorted(self.selectedIndexes())
+
+            copy_text = ''
+            max_column = copied_cells[-1].column()
+            for c in copied_cells:
+                copy_text += self.item(c.row(), c.column()).text()
+                if c.column() == max_column:
+                    copy_text += '\n'
+                else:
+                    copy_text += '\t'
+
+            QApplication.clipboard().setText(copy_text)
+
+
+def update_table(table, data=None):
+    table.setRowCount(0)
+    table.horizontalHeader().setVisible(False)
+
+    if data is None:
+        return table
+
+    _data = data.copy()
+    m = len(_data['timestamp_raw'])
+    del _data['timestamp_raw']
+    del _data['timestamp']
+    n = len(_data)
+
+    table.setRowCount(m)
+    table.setColumnCount(n)
+    for i, key in enumerate(_data.keys()):
+        for j, v in enumerate(_data[key]):
+            table.setItem(j, i, QTableWidgetItem(f'{v:g}'))
+    table.setHorizontalHeaderLabels(list(_data.keys()))
+    table.horizontalHeader().setVisible(True)
+
+    return table
+
+
+def data_table(data=None):
+    table = TableWithCopy()
+    table.horizontalHeader().setResizeMode(QHeaderView.Stretch)
+    return update_table(table, data)
