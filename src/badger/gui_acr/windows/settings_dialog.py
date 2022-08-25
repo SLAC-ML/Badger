132fd855496f58c97218418cd27ec82ba275f4cf
Zhe Zhang <zhezhang@slac.stanford.edu>
2022-08-25 13:07:57 -0700

    Refine the settings dialog code

--- src/badger/gui_acr/windows/settings_dialog.py
+++ src/badger/gui_acr/windows/settings_dialog.py
@@ -3,7 +3,8 @@ from PyQt5.QtGui import QRegExpValidator
 from PyQt5.QtWidgets import QComboBox, QGridLayout, QVBoxLayout, QWidget, QLabel, QLineEdit
 from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QApplication, QStyledItemDelegate
 from qdarkstyle import load_stylesheet, DarkPalette, LightPalette
-from ...settings import list_settings, read_value, write_value, BADGER_CORE_DICT, BADGER_PATH_DICT
+from ...settings import list_settings, read_value, write_value
+
 
 class BadgerSettingsDialog(QDialog):
     theme_list = ['default', 'light', 'dark']
@@ -23,7 +24,7 @@ class BadgerSettingsDialog(QDialog):
 
     def init_ui(self):
         self.setWindowTitle('Badger settings')
-        self.resize(700, 400)      
+        self.setMinimumWidth(640)
 
         vbox = QVBoxLayout(self)
 
@@ -43,46 +44,46 @@ class BadgerSettingsDialog(QDialog):
         grid.addWidget(lbl_theme, 0, 0)
         grid.addWidget(cb_theme, 0, 1)
 
-        # Plugin Root    
-        self.plugin_root = plugin_root = QLabel('Plugin Root:')
+        # Plugin Root
+        self.plugin_root = plugin_root = QLabel('Plugin Root')
         self.plugin_root_path = plugin_root_path = QLineEdit(read_value('BADGER_PLUGIN_ROOT'))
         grid.addWidget(plugin_root, 1, 0)
         grid.addWidget(plugin_root_path, 1, 1)
 
         # DB Root
-        self.db_root = db_root = QLabel('DB Root:')
+        self.db_root = db_root = QLabel('Database Root')
         self.db_root_path = db_root_path = QLineEdit(read_value('BADGER_DB_ROOT'))
         grid.addWidget(db_root, 2, 0)
-        grid.addWidget(db_root_path, 2, 1)   
+        grid.addWidget(db_root_path, 2, 1)
 
         # Logbook Root
-        self.logbook_root = logbook_root = QLabel('Logbook Root:')
+        self.logbook_root = logbook_root = QLabel('Logbook Root')
         self.logbook_root_path = logbook_root_path = QLineEdit(read_value('BADGER_LOGBOOK_ROOT'))
         grid.addWidget(logbook_root, 3, 0)
-        grid.addWidget(logbook_root_path, 3, 1)  
+        grid.addWidget(logbook_root_path, 3, 1)
 
-        # Archive Root      
-        self.archive_root = archive_root = QLabel('Archive Root:')
+        # Archive Root
+        self.archive_root = archive_root = QLabel('Archive Root')
         self.archive_root_path = archive_root_path = QLineEdit(read_value('BADGER_ARCHIVE_ROOT'))
         grid.addWidget(archive_root, 4, 0)
         grid.addWidget(archive_root_path, 4, 1)
 
-        # Check Variable Interval     
-        self.var_int = var_int = QLabel('Check Variable Interval:')
+        # Check Variable Interval
+        self.var_int = var_int = QLabel('Check Variable Interval')
         self.var_int_val = var_int_val = QLineEdit(str(read_value('BADGER_CHECK_VAR_INTERVAL')))
         self.var_int_val.setValidator(validator)
         grid.addWidget(var_int, 5, 0)
         grid.addWidget(var_int_val, 5, 1)
 
-        # Check Variable Timeout     
-        self.var_time = var_time = QLabel('Check Variable Timeout:')
+        # Check Variable Timeout
+        self.var_time = var_time = QLabel('Check Variable Timeout')
         self.var_time_val = var_time_val = QLineEdit(str(read_value('BADGER_CHECK_VAR_TIMEOUT')))
         self.var_time_val.setValidator(validator)
         grid.addWidget(var_time, 6, 0)
         grid.addWidget(var_time_val, 6, 1)
 
         # Plugin URL
-        self.plugin_url = plugin_url = QLabel('Plugin Installation URL:')
+        self.plugin_url = plugin_url = QLabel('Plugin Server URL')
         self.plugin_url_name = plugin_url_name = QLineEdit(read_value('BADGER_PLUGINS_URL'))
         grid.addWidget(plugin_url, 7, 0)
         grid.addWidget(plugin_url_name, 7, 1)
@@ -118,7 +119,7 @@ class BadgerSettingsDialog(QDialog):
         # Update the internal settings
         write_value('BADGER_THEME', theme)
 
-    def apply_settings(self):   
+    def apply_settings(self):
         self.accept()
         write_value('BADGER_PLUGIN_ROOT', self.plugin_root_path.text())
         write_value('BADGER_DB_ROOT', self.db_root_path.text())
@@ -139,13 +140,3 @@ class BadgerSettingsDialog(QDialog):
             write_value(key, self.settings[key])
 
         self.reject()
-
-    
-
-
-
-
-
-
-
-
