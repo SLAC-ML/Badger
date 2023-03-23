3c54bc4a626248a9a5e54637dcede6cbf0b9dafe
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-03-23 14:20:12 -0700

    Support render docs as plain text

--- src/badger/gui/default/windows/docs_window.py
+++ src/badger/gui/default/windows/docs_window.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QTextEdit, QVBoxLayout, QWidget, QMainWindow
+from PyQt5.QtWidgets import QTextEdit, QHBoxLayout, QVBoxLayout, QCheckBox, QWidget, QMainWindow
 from ....factory import get_algo_docs
 
 
@@ -7,8 +7,11 @@ class BadgerDocsWindow(QMainWindow):
         super().__init__(parent=parent)
 
         self.algo = algo
+        self.render_md = True
+        self.docs = None
 
         self.init_ui()
+        self.config_logic()
         self.load_docs()
 
     def init_ui(self):
@@ -17,21 +20,42 @@ class BadgerDocsWindow(QMainWindow):
 
         doc_panel = QWidget(self)
         vbox = QVBoxLayout(doc_panel)
+
+        # Toolbar
+        toolbar = QWidget()
+        hbox_tool = QHBoxLayout(toolbar)
+        hbox_tool.setContentsMargins(0, 0, 0, 0)
+        self.cb_md = cb_md = QCheckBox('Render as Markdown')
+        cb_md.setChecked(True)
+        hbox_tool.addStretch()
+        hbox_tool.addWidget(cb_md)
+        vbox.addWidget(toolbar)
+
         self.markdown_viewer = QTextEdit()
         self.markdown_viewer.setReadOnly(True)
         vbox.addWidget(self.markdown_viewer)
 
         self.setCentralWidget(doc_panel)
 
+    def config_logic(self):
+        self.cb_md.stateChanged.connect(self.switch_render_mode)
+
     def load_docs(self):
         try:
-            docs = get_algo_docs(self.algo)
+            self.docs = docs = get_algo_docs(self.algo)
         except Exception as e:
-            docs = str(e)
+            self.docs = docs = str(e)
 
-        self.markdown_viewer.setMarkdown(docs)
+        if self.render_md:
+            self.markdown_viewer.setMarkdown(docs)
+        else:
+            self.markdown_viewer.setText(docs)
 
     def update_docs(self, algo):
         self.algo = algo
         self.setWindowTitle(f'Docs for algorithm {algo}')
+        self.load_docs()
+
+    def switch_render_mode(self):
+        self.render_md = self.cb_md.isChecked()
         self.load_docs()
