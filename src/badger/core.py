ffd6baf605ca14cfd9d63749c8c97591bb7e28e6
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-09-01 15:00:13 -0700

    Fix code formating issues

--- src/badger/core.py
+++ src/badger/core.py
@@ -1,6 +1,7 @@
 import time
 import numpy as np
 import logging
+
 logger = logging.getLogger(__name__)
 from operator import itemgetter
 from typing import Callable
@@ -8,19 +9,28 @@ from pandas import DataFrame, concat
 from pydantic import BaseModel
 from xopt import Generator
 from .environment import Environment
-from .utils import range_to_str, yprint, merge_params, ParetoFront, norm, denorm, \
-     parse_rule, curr_ts_to_str, dump_state
+from .utils import (
+    range_to_str,
+    yprint,
+    merge_params,
+    ParetoFront,
+    norm,
+    denorm,
+    parse_rule,
+    curr_ts_to_str,
+    dump_state,
+)
 
 
 def process_raw(raw, rule):
     # filter = rule['filter']
-    reducer = rule['reducer']
+    reducer = rule["reducer"]
 
-    if reducer == 'percentile_80':
+    if reducer == "percentile_80":
         return np.percentile(raw, 80)
-    elif reducer == 'mean':
+    elif reducer == "mean":
         return np.mean(raw)
-    elif reducer == 'median':
+    elif reducer == "median":
         return np.median(raw)
 
 
@@ -35,13 +45,13 @@ def normalize_routine(routine):
     #  env_params
     #  env_vranges: use for vars normalization in config
     #  config: change the var ranges, the obj rules, and the constraints
-    env_vranges = routine['env_vranges']
-    config = routine['config']
+    env_vranges = routine["env_vranges"]
+    config = routine["config"]
 
     # Normalize the variables
-    for i, var in enumerate(config['variables']):
+    for i, var in enumerate(config["variables"]):
         if type(var) is str:
-            config['variables'][i] = _dict = {}
+            config["variables"][i] = _dict = {}
             _dict[var] = env_vranges[var]
         else:
             var_name = next(iter(var))  # get the only (first) key in the dict
@@ -54,85 +64,96 @@ def normalize_routine(routine):
 
             if vrange_vocs[0] < vrange_default[0]:
                 logger.warning(
-                    f'variable {var_name}: lower limit {vrange_vocs[0]} exceeds the bound, set to the lower bound {vrange_default[0]}')
+                    f"variable {var_name}: lower limit {vrange_vocs[0]} exceeds the bound, set to the lower bound {vrange_default[0]}"
+                )
                 lb = vrange_default[0]
             else:
                 lb = vrange_vocs[0]
             if vrange_vocs[1] > vrange_default[1]:
                 logger.warning(
-                    f'variable {var_name}: upper limit {vrange_vocs[1]} exceeds the bound, set to the upper bound {vrange_default[1]}')
+                    f"variable {var_name}: upper limit {vrange_vocs[1]} exceeds the bound, set to the upper bound {vrange_default[1]}"
+                )
                 ub = vrange_default[1]
             else:
                 ub = vrange_vocs[1]
 
             if lb >= ub:  # TODO: add logic to deal with the == condition
                 raise Exception(
-                    f'variable {var_name}: lower limit {lb} must be lower than the upper limit {ub}!')
+                    f"variable {var_name}: lower limit {lb} must be lower than the upper limit {ub}!"
+                )
             var[var_name] = [lb, ub]
 
     # Normalize the objectives
-    for i, obj in enumerate(config['objectives']):
+    for i, obj in enumerate(config["objectives"]):
         if type(obj) is str:
-            config['objectives'][i] = _dict = {}
-            _dict[obj] = 'MINIMIZE'
+            config["objectives"][i] = _dict = {}
+            _dict[obj] = "MINIMIZE"
         else:
             obj_name = next(iter(obj))
             if obj[obj_name] is None:
-                obj[obj_name] = 'MINIMIZE'
+                obj[obj_name] = "MINIMIZE"
 
     # Normalize the constraints
     try:
-        _ = config['constraints']
+        _ = config["constraints"]
         if not _:  # empty list
-            config['constraints'] = None
+            config["constraints"] = None
     except KeyError:
-        config['constraints'] = None
+        config["constraints"] = None
 
     # Normalize the states
     try:
-        _ = config['states']
+        _ = config["states"]
         if not _:  # empty list
-            config['states'] = None
+            config["states"] = None
     except KeyError:
-        config['states'] = None
+        config["states"] = None
 
     # Normalize the domain scaling
     try:
-        _ = config['domain_scaling']
+        _ = config["domain_scaling"]
         if not _:  # empty dict
-            config['domain_scaling'] = None
+            config["domain_scaling"] = None
     except KeyError:
-        config['domain_scaling'] = None
+        config["domain_scaling"] = None
 
     # Remove the additional info
-    del routine['env_vranges']
+    del routine["env_vranges"]
 
     return routine
 
 
-def run_routine(routine, skip_review=False, save=None, verbose=2,
-                before_evaluate=None, after_evaluate=None,
-                env_ready=None, pf_ready=None, states_ready=None):
+def run_routine(
+    routine,
+    skip_review=False,
+    save=None,
+    verbose=2,
+    before_evaluate=None,
+    after_evaluate=None,
+    env_ready=None,
+    pf_ready=None,
+    states_ready=None,
+):
     # Review the routine
     if not skip_review:
-        print('Please review the routine to be run:\n')
+        print("Please review the routine to be run:\n")
 
-        routine_configs_var = routine['config']['variables']
-        routine['config']['variables'] = range_to_str(routine_configs_var)
-        print('=== Optimization Routine ===')
+        routine_configs_var = routine["config"]["variables"]
+        routine["config"]["variables"] = range_to_str(routine_configs_var)
+        print("=== Optimization Routine ===")
         yprint(routine)
-        print('')
+        print("")
 
         while True:
-            res = input('Proceed ([y]/n)? ')
-            if res == 'n':
+            res = input("Proceed ([y]/n)? ")
+            if res == "n":
                 return
-            elif (not res) or (res == 'y'):
+            elif (not res) or (res == "y"):
                 break
             else:
-                print(f'Invalid choice: {res}')
+                print(f"Invalid choice: {res}")
 
-        routine['config']['variables'] = routine_configs_var
+        routine["config"]["variables"] = routine_configs_var
 
     # Save routine if specified
     if save:
@@ -143,15 +164,15 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
             save_routine(routine)
         except sqlite3.IntegrityError:
             raise Exception(
-                f'Routine {routine["name"]} already existed in the database! Please choose another name.')
+                f'Routine {routine["name"]} already existed in the database! Please choose another name.'
+            )
 
     # Set up and run the optimization
     from .factory import get_algo, get_env
 
     # Instantiate the environment
-    Environment, configs_env = get_env(routine['env'])
-    _configs_env = merge_params(
-        configs_env, {'params': routine['env_params']})
+    Environment, configs_env = get_env(routine["env"])
+    _configs_env = merge_params(configs_env, {"params": routine["env_params"]})
 
     env = instantiate_env(Environment, _configs_env)
 
@@ -162,43 +183,40 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
     if env_ready:
         env_ready(env)
 
-    optimize, configs_algo = get_algo(routine['algo'])
+    optimize, configs_algo = get_algo(routine["algo"])
 
     from .logger import _get_default_logger
     from .logger.event import Events
 
     # log the optimization progress
     opt_logger = _get_default_logger(verbose)
-    var_names = [next(iter(d)) for d in routine['config']['variables']]
-    vranges = np.array([d[next(iter(d))]
-                        for d in routine['config']['variables']])
-    obj_names = [next(iter(d)) for d in routine['config']['objectives']]
-    rules = [d[next(iter(d))] for d in routine['config']['objectives']]
-    directions = [parse_rule(rule)['direction'] for rule in rules]
+    var_names = [next(iter(d)) for d in routine["config"]["variables"]]
+    vranges = np.array([d[next(iter(d))] for d in routine["config"]["variables"]])
+    obj_names = [next(iter(d)) for d in routine["config"]["objectives"]]
+    rules = [d[next(iter(d))] for d in routine["config"]["objectives"]]
+    directions = [parse_rule(rule)["direction"] for rule in rules]
     pf = ParetoFront(directions)
     if pf_ready:
         pf_ready(pf)
-    if routine['config']['constraints']:
-        con_names = [next(iter(d))
-                        for d in routine['config']['constraints']]
-        thresholds = [d[next(iter(d))]
-                        for d in routine['config']['constraints']]
+    if routine["config"]["constraints"]:
+        con_names = [next(iter(d)) for d in routine["config"]["constraints"]]
+        thresholds = [d[next(iter(d))] for d in routine["config"]["constraints"]]
     else:
         con_names = []
         thresholds = []
     try:
-        sta_names = routine['config']['states'] or []
+        sta_names = routine["config"]["states"] or []
     except KeyError:  # this would happen when rerun an old version routine
         sta_names = []
 
     # Domain scaling for safety
     try:
-        configs_scaling = routine['config']['domain_scaling']
+        configs_scaling = routine["config"]["domain_scaling"]
     except KeyError:  # this would happen when rerun an old version routine
         configs_scaling = None
     scaling = get_scaling_func(configs_scaling)
 
-    info = {'count': -1}
+    info = {"count": -1}
     # Make a normalized evaluate function
 
     def evaluate(X):
@@ -220,7 +238,9 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
         X = scaling(X)
         # Double Check if bounds are violated
         if np.max(X) > 1 or np.min(X) < 0:
-            logger.warning('proposed trial solution exceeds the bounds, solution has been clipped at bounds!')
+            logger.warning(
+                "proposed trial solution exceeds the bounds, solution has been clipped at bounds!"
+            )
             X = np.clip(X, 0, 1)
 
         for x in X:
@@ -252,7 +272,7 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
                     obs = float(obs_raw)
                 except:  # obs_raw is a list
                     obs = process_raw(obs_raw, rule_dict)
-                if rule_dict['direction'] == 'MAXIMIZE':
+                if rule_dict["direction"] == "MAXIMIZE":
                     obses.append(-obs)
                 else:
                     obses.append(obs)
@@ -268,9 +288,9 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
             for i, con_name in enumerate(con_names):
                 relation, thres = thresholds[i][:2]
                 con = float(env._get_observables([con_name])[con_name])
-                if relation == 'GREATER_THAN':
+                if relation == "GREATER_THAN":
                     cons_i.append(con - thres)
-                elif relation == 'LESS_THAN':
+                elif relation == "LESS_THAN":
                     cons_i.append(thres - con)
                 else:
                     cons_e.append(con - thres)
@@ -291,12 +311,21 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
                     sta = env._get_variables([sta_name])[sta_name]
                 states.append(sta)
 
-            info['count'] += 1
-            _idx_x = np.insert(_xo, 0, info['count'])  # keep the idx info
+            info["count"] += 1
+            _idx_x = np.insert(_xo, 0, info["count"])  # keep the idx info
 
             is_optimal = not pf.is_dominated((_idx_x, obses_raw))
-            solution = (_xo, obses_raw, cons_raw, states, is_optimal,
-                        var_names, obj_names, con_names, sta_names)
+            solution = (
+                _xo,
+                obses_raw,
+                cons_raw,
+                states,
+                is_optimal,
+                var_names,
+                obj_names,
+                con_names,
+                sta_names,
+            )
             opt_logger.update(Events.OPTIMIZATION_STEP, solution)
 
             if after_evaluate:
@@ -316,20 +345,31 @@ def run_routine(routine, skip_review=False, save=None, verbose=2,
         return Y, I, E, Xo
 
     # Start the optimization
-    print('')
-    solution = (None, None, None, None, None,
-                var_names, obj_names, con_names, sta_names)
+    print("")
+    solution = (
+        None,
+        None,
+        None,
+        None,
+        None,
+        var_names,
+        obj_names,
+        con_names,
+        sta_names,
+    )
     opt_logger.update(Events.OPTIMIZATION_START, solution)
     try:
         if not callable(optimize):  # doing optimization through extensions
             configs = {
-                'routine_configs': routine['config'],
-                'algo_configs': merge_params(configs_algo, {'params': routine['algo_params']}),
-                'env_configs': _configs_env,
+                "routine_configs": routine["config"],
+                "algo_configs": merge_params(
+                    configs_algo, {"params": routine["algo_params"]}
+                ),
+                "env_configs": _configs_env,
             }
             optimize = optimize.optimize
         else:
-            configs = routine['algo_params']
+            configs = routine["algo_params"]
 
         # Save system states if applicable
         states = env.get_system_states()
@@ -351,7 +391,7 @@ def instantiate_env(env_class, configs, manager=None):
     # It seems that the interface should be given rather than
     # initialized here
     try:
-        intf_name = configs['interface'][0]
+        intf_name = configs["interface"][0]
     except KeyError:
         intf_name = None
     except Exception as e:
@@ -367,7 +407,7 @@ def instantiate_env(env_class, configs, manager=None):
     else:
         intf = None
 
-    env = env_class(interface=intf, **configs['params'])
+    env = env_class(interface=intf, **configs["params"])
 
     return env
 
@@ -375,63 +415,63 @@ def instantiate_env(env_class, configs, manager=None):
 # The following functions are related to domain scaling
 # TODO: consider combine them into a class and make it extensible
 def list_scaling_func():
-    return ['semi-linear', 'sinusoid', 'sigmoid']
+    return ["semi-linear", "sinusoid", "sigmoid"]
 
 
 def get_scaling_default_params(name):
-    if name == 'semi-linear':
+    if name == "semi-linear":
         default_params = {
-            'center': 0.5,
-            'range': 1,
+            "center": 0.5,
+            "range": 1,
         }
-    elif name == 'sinusoid':
+    elif name == "sinusoid":
         default_params = {
-            'center': 0.5,
-            'period': 2,
+            "center": 0.5,
+            "period": 2,
         }
-    elif name == 'sigmoid':
+    elif name == "sigmoid":
         default_params = {
-            'center': 0.5,
-            'lambda': 8,
+            "center": 0.5,
+            "lambda": 8,
         }
     else:
-        raise Exception(f'scaling function {name} is not supported')
+        raise Exception(f"scaling function {name} is not supported")
 
     return default_params
 
 
 def get_scaling_func(configs):
     if not configs:  # fallback to default
-        configs = {'func': 'semi-linear'}
+        configs = {"func": "semi-linear"}
 
-    name = configs['func']
+    name = configs["func"]
     params = configs.copy()
-    del params['func']
+    del params["func"]
 
     default_params = get_scaling_default_params(name)
     params = merge_params(default_params, params)
 
-    if name == 'semi-linear':
-        center, range = itemgetter('center', 'range')(params)
+    if name == "semi-linear":
+        center, range = itemgetter("center", "range")(params)
 
         def func(X):
             return np.clip((X - center) / range + 0.5, 0, 1)
 
-    elif name == 'sinusoid':
-        center, period = itemgetter('center', 'period')(params)
+    elif name == "sinusoid":
+        center, period = itemgetter("center", "period")(params)
 
         def func(X):
             return 0.5 * np.sin(2 * np.pi / period * (X - center)) + 0.5
 
-    elif name == 'sigmoid':
-        center, lamb = itemgetter('center', 'lambda')(params)
+    elif name == "sigmoid":
+        center, lamb = itemgetter("center", "lambda")(params)
 
         def func(X):
             return 1 / (1 + np.exp(-lamb * (X - center)))
 
     # TODO: consider remove this branch since it's useless
     else:
-        raise Exception(f'scaling function {name} is not supported')
+        raise Exception(f"scaling function {name} is not supported")
 
     return func
 
@@ -456,8 +496,9 @@ class Routine(BaseModel):
         return self.generator.vocs
 
 
-def evaluate_points(points: DataFrame, routine: Routine, callback: Callable) \
-        -> DataFrame:
+def evaluate_points(
+    points: DataFrame, routine: Routine, callback: Callable
+) -> DataFrame:
     """
     Evaluates points using the environment
 
@@ -491,14 +532,14 @@ def evaluate_points(points: DataFrame, routine: Routine, callback: Callable) \
 
 
 def run_routine_xopt(
-        routine: Routine,
-        active_callback: Callable,
-        generate_callback: Callable,
-        evaluate_callback: Callable,
-        pf_callback: Callable,
-        states_callback: Callable,
-        dump_file_callback: Callable = None,
-        ) -> None:
+    routine: Routine,
+    active_callback: Callable,
+    generate_callback: Callable,
+    evaluate_callback: Callable,
+    pf_callback: Callable,
+    states_callback: Callable,
+    dump_file_callback: Callable = None,
+) -> None:
     """
     Run the provided routine object using Xopt.
 
@@ -534,7 +575,9 @@ def run_routine_xopt(
     initial_points = routine.initial_points
 
     # Setup Pareto front: soon to die
-    directions = [parse_rule(rule)['direction'] for rule in routine.vocs.objectives.values()]
+    directions = [
+        parse_rule(rule)["direction"] for rule in routine.vocs.objectives.values()
+    ]
     pf = ParetoFront(directions)
     if pf_callback:
         pf_callback(pf)
@@ -557,13 +600,13 @@ def run_routine_xopt(
         ts_start = curr_ts_to_str()
         dump_file = dump_file_callback()
         if not dump_file:
-            dump_file = f'xopt_states_{ts_start}.yaml'
+            dump_file = f"xopt_states_{ts_start}.yaml"
 
     # perform optimization
     while True:
         status = active_callback()
         if status == 2:
-            raise Exception('Optimization run has been terminated!')
+            raise Exception("Optimization run has been terminated!")
         elif status == 1:
             time.sleep(0)
             continue
