3dbf6797e82ee54b18a063a6e7e7a9673a396758
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-09-11 14:00:20 -0700

    Reformat interface-related files wrt ufmt

--- src/badger/tests/test_intf.py
+++ src/badger/tests/test_intf.py
@@ -1,26 +1,26 @@
 def test_find_intf():
-    from badger.factory import list_intf, get_intf
+    from badger.factory import get_intf, list_intf
 
     assert len(list_intf()) == 1
 
-    _, configs = get_intf('test')
-    assert configs['name'] == 'test'
-    assert configs['version'] == '1.0'
+    _, configs = get_intf("test")
+    assert configs["name"] == "test"
+    assert configs["version"] == "1.0"
 
 
 def test_get_params():
     from badger.factory import get_intf
 
-    Interface, configs = get_intf('test')
-    params = configs['params']
+    _, configs = get_intf("test")
+    params = configs["params"]
 
-    assert params == {'flag': 0}
+    assert params == {"flag": 0}
 
 
 def test_set_params():
     from badger.factory import get_intf
 
-    Interface, configs = get_intf('test')
+    Interface, _ = get_intf("test")
     intf = Interface(flag=1)
 
     assert intf.flag == 1
@@ -29,54 +29,55 @@ def test_set_params():
 def test_get_values():
     from badger.factory import get_intf
 
-    Interface, _ = get_intf('test')
+    Interface, _ = get_intf("test")
     intf = Interface()
 
-    channel_outputs = intf.get_values(['x1', 'x2'])
-    assert channel_outputs == {'x1': 0, 'x2': 0}
+    channel_outputs = intf.get_values(["x1", "x2"])
+    assert channel_outputs == {"x1": 0, "x2": 0}
 
     # Test single version
-    value = intf.get_value('x3')
+    value = intf.get_value("x3")
     assert value == 0
 
 
 def test_set_values():
     from badger.factory import get_intf
 
-    Interface, _ = get_intf('test')
+    Interface, _ = get_intf("test")
     intf = Interface()
 
-    channel_inputs = {'x1': 3, 'x2': 4}
+    channel_inputs = {"x1": 3, "x2": 4}
     intf.set_values(channel_inputs)
-    channel_outputs = intf.get_values(['x1', 'x2', 'x3'])
-    assert channel_outputs == {'x1': 3, 'x2': 4, 'x3': 0}
+    channel_outputs = intf.get_values(["x1", "x2", "x3"])
+    assert channel_outputs == {"x1": 3, "x2": 4, "x3": 0}
 
     # Test single version
-    intf.set_value('x3', 5)
-    value = intf.get_value('x3')
+    intf.set_value("x3", 5)
+    value = intf.get_value("x3")
     assert value == 5
 
 
 def test_recording():
     from badger.factory import get_intf
 
-    Interface, _ = get_intf('test')
+    Interface, _ = get_intf("test")
     intf = Interface()
 
-    intf.set_values({'x1': 3, 'x2': 4})
-    intf.set_value('x3', 5)
-    intf.get_values(['x1', 'x2', 'x3'])
+    intf.set_values({"x1": 3, "x2": 4})
+    intf.set_value("x3", 5)
+    intf.get_values(["x1", "x2", "x3"])
 
     assert len(intf._logs) == 3
     record = intf._logs[0]
-    assert record['action'] == 'set_values'
-    assert record['channel_inputs'] == {'x1': 3, 'x2': 4}
+    assert record["action"] == "set_values"
+    assert record["channel_inputs"] == {"x1": 3, "x2": 4}
     record = intf._logs[1]
-    assert record['action'] == 'set_values'
-    assert record['channel_inputs'] == {'x3': 5}
+    assert record["action"] == "set_values"
+    assert record["channel_inputs"] == {"x3": 5}
     record = intf._logs[2]
-    assert record['action'] == 'get_values'
-    assert record['channel_outputs'] == {'x1': 3, 'x2': 4, 'x3': 5}
+    assert record["action"] == "get_values"
+    assert record["channel_outputs"] == {"x1": 3, "x2": 4, "x3": 5}
+
 
 # def test_run(mock_config_root):
 #     from coolname import generate_slug
