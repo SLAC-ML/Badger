61a7b597a256e0d3799139525064b1fa3622bc41
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-09-14 15:00:34 -0700

    Simplify the test env

--- src/badger/tests/mock/plugins/environments/test/__init__.py
+++ src/badger/tests/mock/plugins/environments/test/__init__.py
@@ -9,12 +9,7 @@ class Environment(environment.Environment):
     variables = {f'x{i}': [-1, 1] for i in range(20)}
     observables = ['f']
 
-    def get_variables(self, variable_names):
-        assert self.interface, 'Must provide an interface!'
-
-        variable_outputs = self.interface.get_values(variable_names)
-
-        return variable_outputs
+    flag = 0
 
     def set_variables(self, variable_inputs: Dict[str, float]):
         assert self.interface, 'Must provide an interface!'
@@ -25,8 +20,3 @@ class Environment(environment.Environment):
         # Filling up the observations
         x = torch.tensor([full_outputs[f'x{i}'] for i in range(20)])
         self.interface.set_value('f', (x ** 2).sum().numpy())
-
-    def get_observables(self, observable_names):
-        assert self.interface, 'Must provide an interface!'
-
-        return self.interface.get_values(observable_names)
