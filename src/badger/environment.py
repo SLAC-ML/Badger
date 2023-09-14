d80e41a8e6c5d4be29db52c49c5fcc2cb89cd22f
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-09-14 15:59:04 -0700

    Format the env code to meet Flake8 standard

--- src/badger/environment.py
+++ src/badger/environment.py
@@ -1,20 +1,25 @@
-from abc import ABC, abstractmethod
-from typing import List, Dict, Optional, final, ClassVar
+from abc import ABC
+from typing import ClassVar, Dict, final, List, Optional
+
 from pydantic import BaseModel
-from .interface import Interface
+
 from .errors import (
+    BadgerEnvObsError,
     BadgerEnvVarError,
-    BadgerIntfChannelError,
-    BadgerEnvObsError
+    BadgerIntfChannelError
 )
+from .interface import Interface
 
 
 def validate_variable_names(func):
     def validate(cls, variable_names: List[str]):
-        variable_names_invalid = [name for name in variable_names
-                                  if name not in cls.variables]
+        variable_names_invalid = [
+            name for name in variable_names if name not in cls.variables
+        ]
         if len(variable_names_invalid):
-            raise BadgerEnvVarError(f"Variables {variable_names_invalid} not found in environment")
+            raise BadgerEnvVarError(
+                f"Variables {variable_names_invalid} not found in environment"
+            )
 
         return func(cls, variable_names)
 
@@ -23,10 +28,14 @@ def validate_variable_names(func):
 
 def validate_observable_names(func):
     def validate(cls, observable_names: List[str]):
-        observable_names_invalid = [name for name in observable_names
-                                    if name not in cls.observables]
+        observable_names_invalid = [
+            name for name in observable_names if name not in cls.observables
+        ]
         if len(observable_names_invalid):
-            raise BadgerEnvObsError(f"Observables {observable_names_invalid} not found in environment")
+            raise BadgerEnvObsError(
+                f"Observables {observable_names_invalid} "
+                + "not found in environment"
+            )
 
         return func(cls, observable_names)
 
@@ -37,12 +46,14 @@ def validate_setpoints(func):
     def validate(cls, variable_inputs: Dict[str, float]):
         _bounds = cls._get_bounds(list(variable_inputs.keys()))
         for name, value in variable_inputs.items():
-
             lower = _bounds[name][0]
             upper = _bounds[name][1]
 
             if value > upper or value < lower:
-                raise BadgerEnvVarError(f"Input point for {name} is outside its bounds {_bounds[name]}")
+                raise BadgerEnvVarError(
+                    f"Input point for {name} is outside "
+                    + f"its bounds {_bounds[name]}"
+                )
 
         return func(cls, variable_inputs)
 
@@ -50,7 +61,6 @@ def validate_setpoints(func):
 
 
 class Environment(BaseModel, ABC):
-
     # Class variables
     name: ClassVar[str]
     variables: ClassVar[Dict[str, List]]  # bounds list could be empty for var
@@ -72,17 +82,17 @@ class Environment(BaseModel, ABC):
     ############################################################
 
     def get_variables(self, variable_names: List[str]) -> Dict:
-        assert self.interface, 'Must provide an interface!'
+        assert self.interface, "Must provide an interface!"
 
         return self.interface.get_values(variable_names)
 
     def set_variables(self, variable_inputs: Dict[str, float]):
-        assert self.interface, 'Must provide an interface!'
+        assert self.interface, "Must provide an interface!"
 
         return self.interface.set_values(variable_inputs)
 
     def get_observables(self, observable_names: List[str]) -> Dict:
-        assert self.interface, 'Must provide an interface!'
+        assert self.interface, "Must provide an interface!"
 
         return self.interface.get_values(observable_names)
 
@@ -117,8 +127,8 @@ class Environment(BaseModel, ABC):
 
         # Deal with tmp variables
         # Usually should be able to read from the interface
-        variable_names_tmp = [v for v in variable_names
-                              if v not in self.variables]
+        variable_names_tmp = [v for v in variable_names if
+                              v not in self.variables]
 
         # If no undefined variables
         if not len(variable_names_tmp):
@@ -126,13 +136,20 @@ class Environment(BaseModel, ABC):
 
         # If no interface
         if self.interface is None:
-            raise BadgerEnvVarError(f'Variables {variable_names_tmp} do not exist in the environment!')
+            raise BadgerEnvVarError(
+                f"Variables {variable_names_tmp} " +
+                "do not exist in the environment!"
+            )
 
         # Try reading variable values from the interface
         try:
-            variable_outputs_tmp = self.interface.get_values(variable_names_tmp)
+            variable_outputs_tmp = self.interface.get_values(
+                variable_names_tmp)
         except Exception:  # TODO: specify what exceptions could occur
-            raise BadgerIntfChannelError(f'Error reading variables {variable_names_tmp} from the interface!')
+            raise BadgerIntfChannelError(
+                f"Error reading variables {variable_names_tmp} "
+                + "from the interface!"
+            )
 
         return {**variable_outputs_def, **variable_outputs_tmp}
 
@@ -148,14 +165,16 @@ class Environment(BaseModel, ABC):
     @final
     def _set_variables(self, variable_inputs: Dict[str, float]):
         # Deal with variables defined in env
-        variable_inputs_def = dict([v for v in variable_inputs.items()
-                                    if v[0] in self.variables])
+        variable_inputs_def = dict(
+            [v for v in variable_inputs.items() if v[0] in self.variables]
+        )
         self._set_variables_def(variable_inputs_def)
 
         # Deal with tmp variables
         # Usually should be able to directly set to the interface
-        variable_inputs_tmp = dict([v for v in variable_inputs.items()
-                                    if v[0] not in self.variables])
+        variable_inputs_tmp = dict(
+            [v for v in variable_inputs.items() if v[0] not in self.variables]
+        )
         variable_names_tmp = list(variable_inputs_tmp.keys())
 
         # If no undefined variables
@@ -164,11 +183,19 @@ class Environment(BaseModel, ABC):
 
         # If no interface
         if self.interface is None:
-            raise BadgerEnvVarError(f'Variables {variable_names_tmp} do not exist in the environment!')
+            err_msg = (
+                f"Variables [{variable_names_tmp}] "
+                + "do not exist in the environment!"
+            )
+            raise BadgerEnvVarError(err_msg)
 
         # Heads-up to the users that this behavior is not allowed for now
-        raise BadgerIntfChannelError(f'Variables {variable_names_tmp} not defined in the environment! ' +
-                                     'Setting them through interface is not allowed for safety consideration.')
+        err_msg = (
+            f"Variables [{variable_names_tmp}] "
+            + "not defined in the environment! "
+            + "Setting them through interface is not allowed."
+        )
+        raise BadgerIntfChannelError(err_msg)
 
     # Optimizer will only call this method to get observable values
     @final
@@ -181,12 +208,15 @@ class Environment(BaseModel, ABC):
     # TODO: considering cache validation (is it needed?)
     @final
     @validate_variable_names
-    def _get_bounds(self, variable_names: Optional[List[str]] = None) -> Dict[str, List[float]]:
+    def _get_bounds(
+        self, variable_names: Optional[List[str]] = None
+    ) -> Dict[str, List[float]]:
         if variable_names is None:
             variable_names = self.variable_names
 
-        variable_names_new = [name for name in variable_names
-                              if not len(self.variables[name])]
+        variable_names_new = [
+            name for name in variable_names if not len(self.variables[name])
+        ]
         if len(variable_names_new):
             self.variables.update(self.get_bounds(variable_names_new))
 
