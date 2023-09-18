46c09ae50d4450872e5e07638cb16c950df68113
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-09-18 13:58:46 -0700

    Add strtobool util

--- src/badger/utils.py
+++ src/badger/utils.py
@@ -304,3 +304,23 @@ def state_to_dict(generator, data, include_data=True):
         output["data"] = json.loads(data.to_json())
 
     return output
+
+
+# https://stackoverflow.com/a/18472142
+def strtobool(val):
+    """Convert a string representation of truth to true (1) or false (0).
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    try:
+        val = val.lower()
+    except AttributeError:
+        return val
+
+    if val in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif val in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (val,))
