d63bb12024806a7e46e84eca1617db319d201bca
YektaY <yektay1@gmail.com>
2023-09-25 13:45:03 -0700

    small change

--- run_tests.py
+++ run_tests.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
 
     # Show coverage
     if "--show-cov" in args:
-        args.extend(["--cov=archive_viewer", "--cov-report", "term-missing"])
+        args.extend(["--cov=badger", "--cov-report", "term-missing"])
         args.remove("--show-cov")
 
     print("pytest arguments: {}".format(args))
