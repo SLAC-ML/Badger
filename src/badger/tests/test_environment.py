d5612cbf60156bdf5bb8cf0d7d495231c98a488b
Ryan Roussel <rroussel@slac.stanford.edu>
2023-10-18 16:52:01 -0500

    Update test_environment.py

--- src/badger/tests/test_environment.py
+++ src/badger/tests/test_environment.py
@@ -3,25 +3,25 @@ from typing import Dict, List
 
 from xopt.resources.testing import TEST_VOCS_BASE
 
-from badger.environment import Environment
-
-
-class TestEnvironment(Environment):
-    name = 'test'
-    variables = {f'x{i}': [-1, 1] for i in range(20)}
-    observables = ['f']
-
-    my_flag: int = 0
-
-    def set_variables(self, variable_inputs: Dict[str, float]):
-        pass
-
-    def get_observables(self, observable_names: List[str]) -> Dict:
-        return {ele: 1.0 for ele in observable_names}
-
 
 class TestEnv:
+
     def test_basic_env(self):
+        from badger.environment import Environment
+
+        class TestEnvironment(Environment):
+            name = 'test'
+            variables = {f'x{i}': [-1, 1] for i in range(20)}
+            observables = ['f']
+
+            my_flag: int = 0
+
+            def set_variables(self, variable_inputs: Dict[str, float]):
+                pass
+
+            def get_observables(self, observable_names: List[str]) -> Dict:
+                return {ele: 1.0 for ele in observable_names}
+
         env = TestEnvironment()
         result = dict(env)
         assert result["my_flag"] == 0
@@ -33,9 +33,22 @@ class TestEnv:
         result = json.loads(env.model_dump_json())
         assert result["my_flag"] == 1
 
-
     def test_env_in_routine(self):
         from badger.environment import Environment
+
+        class TestEnvironment(Environment):
+            name = 'test'
+            variables = {f'x{i}': [-1, 1] for i in range(20)}
+            observables = ['f']
+
+            my_flag: int = 0
+
+            def set_variables(self, variable_inputs: Dict[str, float]):
+                pass
+
+            def get_observables(self, observable_names: List[str]) -> Dict:
+                return {ele: 1.0 for ele in observable_names}
+
         from badger.routine import Routine
 
         env = TestEnvironment()
@@ -56,7 +69,6 @@ class TestEnv:
 
     def test_env_from_get_env(self):
         from badger.factory import get_env
-        from badger.environment import Environment
         from badger.routine import Routine
 
         env = get_env("test")
@@ -74,4 +86,3 @@ class TestEnv:
         routine.environment.flag = 1
         result = json.loads(routine.json())
         assert result["environment"]["flag"] == 1
-
