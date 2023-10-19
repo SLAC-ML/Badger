a8e1f568413a62ecf8ef3d07c297b205e4f19376
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-10-18 17:00:16 -0700

    Recover test env and make related tests work

--- src/badger/tests/mock/plugins/environments/test/__init__.py
+++ src/badger/tests/mock/plugins/environments/test/__init__.py
@@ -1,18 +1,17 @@
 import torch
-from typing import Dict, List, Optional
 from badger import environment
 from badger.errors import BadgerNoInterfaceError
 
 
 class Environment(environment.Environment):
 
-    name = 'test_w_interface'
+    name = 'test'
     variables = {f'x{i}': [-1, 1] for i in range(20)}
     observables = ['f']
 
-    flag: Optional[int] = 0
+    flag: int = 0
 
-    def set_variables(self, variable_inputs: Dict[str, float]):
+    def set_variables(self, variable_inputs: dict[str, float]):
         if not self.interface:
             raise BadgerNoInterfaceError
 
@@ -21,7 +20,4 @@ class Environment(environment.Environment):
 
         # Filling up the observations
         x = torch.tensor([full_outputs[f'x{i}'] for i in range(20)])
-        self.interface.set_value('f', (x ** 2).sum().numpy())
-
-    def get_observables(self, observable_names: List[str]) -> Dict:
-        return {ele: 1.0 for ele in observable_names}
+        self.interface.set_value('f', float((x ** 2).sum().numpy()))
--- src/badger/tests/test_routine.py
+++ src/badger/tests/test_routine.py
@@ -1,14 +1,11 @@
 import os
-
 import pytest
-
-from badger.environment import Environment
-from badger.tests.utils import create_routine
-from badger.routine import Routine
 
 
 class TestRoutine:
     def test_routine_init(self):
+        from badger.tests.utils import create_routine
+
         routine = create_routine()
         routine.evaluate_data(routine.initial_points)
 
@@ -16,36 +13,41 @@ class TestRoutine:
 
     def test_routine_serialization(self):
         from badger.routine import Routine
+        from badger.tests.utils import create_routine
 
         routine = create_routine()
         routine.evaluate_data(routine.initial_points)
         routine.dump("test.yaml")
 
         lroutine = Routine.from_file("test.yaml")
-        assert lroutine.environment.variable_names == routine.environment.variable_names
+        assert lroutine.environment.variable_names ==\
+            routine.environment.variable_names
         lroutine.evaluate_data(routine.initial_points)
+
+        print(lroutine.data)
 
         assert len(lroutine.data) == 2
 
     def test_routine_env_dump(self):
+        from badger.tests.utils import create_routine
+        from badger.environment import Environment
+        from badger.routine import Routine
 
         routine = create_routine()
         assert isinstance(routine.environment, Environment)
 
-        routine.environment.my_flag = 1
-        assert routine.environment.my_flag == 1
+        routine.environment.flag = 1
+        assert routine.environment.flag == 1
 
         routine_str = routine.yaml()
-        # print(routine.environment, 'raw env')
         # print(routine_str, 'yaml')
-        # print(routine.json(), 'json')
         routine_re = Routine.from_yaml(routine_str)
         assert routine_re.environment.flag == 1
 
     @pytest.fixture(scope="module", autouse=True)
     def clean_up(self):
         yield
-        files = ["test.yml"]
+        files = ["test.yaml"]
         for f in files:
             if os.path.exists(f):
                 os.remove(f)
