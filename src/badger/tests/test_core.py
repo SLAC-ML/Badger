2375c86c7aeb4c4f7d513b4e8a1b2ac8f587145c
Zhe Zhang <zhezhang@slac.stanford.edu>
2023-10-20 23:35:21 -0700

    Fix core test
    Also merge test_turbo with test_core

--- src/badger/tests/test_core.py
+++ src/badger/tests/test_core.py
@@ -17,7 +17,6 @@ class TestCore:
     def test_core_setup(self, *args, **kwargs) -> None:
         super(TestCore, self).__init__(*args, **kwargs)
         self.count = 0
-        self.points_eval = None
         self.candidates = None
         self.points_eval_list = []
         self.candidates_list = []
@@ -35,116 +34,6 @@ class TestCore:
         self.points = pd.DataFrame(data)
 
         self.points_eval_target = pd.DataFrame(data_eval_target)
-
-        self.test_evaluate_points_cases = [
-            (
-                self.points,
-                self.mock_routine,
-                self.mock_evaluate_points_callback,
-                self.points_eval_target,
-            )
-        ]
-
-    def mock_routine(self):
-        """
-        A method that creates a Routine class object
-        filled with sample data for testing purposes.
-        """
-        from badger.factory import get_env
-        from badger.routine import Routine
-        from badger.environment import instantiate_env
-
-        test_routine = {
-            "name": "routine-for-core-test",
-            "algo": "upper_confidence_bound",
-            "env": "test",
-            "algo_params": {
-                "model": None,
-                "turbo_controller": None,
-                "use_cuda": False,
-                "gp_constructor": {
-                    "name": "standard",
-                    "use_low_noise_prior": True,
-                    "covar_modules": {},
-                    "mean_modules": {},
-                    "trainable_mean_keys": [],
-                },
-                "numerical_optimizer": {
-                    "name": "LBFGS",
-                    "n_restarts": 20,
-                    "max_iter": 2000,
-                },
-                "max_travel_distances": None,
-                "fixed_features": None,
-                "n_candidates": 1,
-                "n_monte_carlo_samples": 128,
-                "beta": 2.0,
-                "start_from_current": True,
-            },
-            "env_params": {},
-            "config": {
-                "variables": [
-                    {"x0": [-1, 1]},
-                    {"x1": [-1, 1]},
-                    {"x2": [-1, 1]},
-                    {"x3": [-1, 1]},
-                ],
-                "objectives": [{"f": "MAXIMIZE"}],
-                "constraints": None,
-                "states": None,
-                "domain_scaling": None,
-                "tags": None,
-                "init_points": {
-                    "x0": [0.5],
-                    "x1": [0.5],
-                    "x2": [0.5],
-                    "x3": [0.5],
-                },
-            },
-        }
-
-        # Initialize routine
-        Environment, configs_env = get_env(test_routine["env"])
-        _configs_env = merge_params(
-            configs_env, {"params": test_routine["env_params"]}
-        )
-        environment = instantiate_env(Environment, _configs_env)
-
-        variables = {
-            key: value
-            for dictionary in test_routine["config"]["variables"]
-            for key, value in dictionary.items()
-        }
-        objectives = {
-            key: value
-            for dictionary in test_routine["config"]["objectives"]
-            for key, value in dictionary.items()
-        }
-        vocs = {
-            "variables": variables,
-            "objectives": objectives,
-        }
-
-        generator_class = get_generator(test_routine["algo"])
-
-        del test_routine["algo_params"]["start_from_current"]
-        del test_routine["algo_params"]["n_candidates"]
-        del test_routine["algo_params"]["fixed_features"]
-
-        test_routine_copy = copy.deepcopy(test_routine["algo_params"])
-
-        generator = generator_class(vocs=vocs, **test_routine_copy)
-
-        initial_points = test_routine["config"]["init_points"]
-        initial_points = pd.DataFrame.from_dict(initial_points)
-
-        test_routine_xopt = Routine(
-            environment=environment,
-            generator=generator,
-            initial_points=initial_points,
-        )
-
-        return test_routine_xopt
 
     def mock_active_callback(self) -> int:
         """
@@ -191,8 +80,11 @@ class TestCore:
         of run_routine_xopt is functioning as intended.
         """
         from badger.core import run_routine
+        from badger.tests.utils import create_routine
 
-        routine = self.mock_routine()
+        routine = create_routine()
+
+        self.count = 0
 
         with pytest.raises(BadgerRunTerminatedError):
             run_routine(
@@ -213,31 +105,47 @@ class TestCore:
         assert os.path.exists(path) is True
         os.remove("./test.yaml")
 
-    def mock_evaluate_points_callback(self, points_eval: pd.DataFrame) -> None:
-        """
-        A mock callback method to test if callback method passed
-        to the evaluate_points method is functioning properly.
-        """
-        self.points_eval = points_eval
-
     def test_evaluate_points(self) -> None:
         """
         A unit test to ensure the core functionality of evaluate_points
         is functioning as intended.
         """
-        routine = self.mock_routine()
+        from badger.tests.utils import create_routine
+
+        routine = create_routine()
 
         assert routine.environment.get_variables(["x1", "x2"]) == {
             "x1": 0,
             "x2": 0,
         }
-        evaluate_points_result = routine.evaluate_data(
-            self.points, routine, self.mock_evaluate_points_callback
+        evaluate_points_result = routine.evaluate_data(self.points)
+
+        vocs_list = ['x0', 'x1', 'x2', 'x3', 'f']
+        assert evaluate_points_result[vocs_list].astype(float).equals(
+            self.points_eval_target.astype(float)
         )
 
-        assert evaluate_points_result.astype(float).equals(
-            self.points_eval_target.astype(float)
-        )
-        assert self.points_eval.astype(float).equals(
-            self.points_eval_target.astype(float)
-        )
+    def test_run_turbo(self) -> None:
+        """
+        A unit test to ensure TuRBO can run in Badger.
+        """
+        from badger.core import run_routine
+        from badger.tests.utils import create_routine_turbo
+
+        routine = create_routine_turbo()
+        assert routine.generator.turbo_controller.best_value is None
+
+        self.count = 0
+
+        with pytest.raises(BadgerRunTerminatedError):
+            run_routine(
+                routine,
+                self.mock_active_callback,
+                self.mock_generate_callback,
+                self.mock_evaluate_callback,
+                self.mock_states_callback,
+                dump_file_callback=None,
+            )
+
+        assert len(self.candidates_list) == self.count - 1
+        assert len(self.points_eval_list) == self.count
--- src/badger/tests/utils.py
+++ src/badger/tests/utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from xopt import VOCS
-from xopt.generators import RandomGenerator
+from xopt.generators import RandomGenerator, UpperConfidenceBoundGenerator
 
 
 def create_routine():
@@ -8,16 +8,9 @@ def create_routine():
 
     test_routine = {
         "name": "routine-for-core-test",
-        "algo": "upper_confidence_bound",
+        "algo": "random",
         "env": "test",
-        "algo_params": {
-            "gp_constructor": {
-                "name": "standard",
-                "use_low_noise_prior": True,
-            },
-            "beta": 2.0,
-            "start_from_current": True,
-        },
+        "algo_params": {},
         "env_params": {},
         "config": {
             "variables": {
@@ -40,6 +33,51 @@ def create_routine():
 
     return Routine(
         name="test",
+        vocs=vocs,
+        generator=generator,
+        environment={"name": "test"},
+        initial_points=pd.DataFrame(test_routine["config"]["init_points"])
+    )
+
+
+def create_routine_turbo():
+    from badger.routine import Routine
+
+    test_routine = {
+        "name": "routine-for-turbo-test",
+        "algo": "upper_confidence_bound",
+        "env": "test",
+        "algo_params": {
+            "turbo_controller": "optimize",
+            "gp_constructor": {
+                "name": "standard",
+                "use_low_noise_prior": True,
+            },
+            "beta": 2.0,
+        },
+        "env_params": {},
+        "config": {
+            "variables": {
+                "x0": [-1, 1],
+                "x1": [-1, 1],
+                "x2": [-1, 1],
+                "x3": [-1, 1]
+            },
+            "objectives": {"f": "MAXIMIZE"},
+            "init_points": {"x0": [0.5], "x1": [0.5], "x2": [0.5], "x3": [0.5]},
+        },
+    }
+
+    vocs = VOCS(
+        variables=test_routine["config"]["variables"],
+        objectives=test_routine["config"]["objectives"],
+    )
+
+    generator = UpperConfidenceBoundGenerator(
+        vocs=vocs, ** test_routine["algo_params"])
+
+    return Routine(
+        name="test-turbo",
         vocs=vocs,
         generator=generator,
         environment={"name": "test"},
